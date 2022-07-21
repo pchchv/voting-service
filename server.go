c5b81b970e0eaf3592915db5bc1d6af7e34b2044
pchchv <ipchchv@gmail.com>
2022-07-21 23:09:02 +0300

    Change the server routes setting

--- server.go
+++ server.go
@@ -8,26 +8,32 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func ping(c *fiber.Ctx) error {
+	msg, err := json.MarshalIndent("Voting Service. Version 0.0.2", "\t", "\t")
+	if err != nil {
+		log.Panic(err)
+	}
+	return c.SendString(string(msg))
+}
+
+func createPoll(c *fiber.Ctx) error {
+	title := c.Query("title")
+	options := strings.Split(c.Query("options"), ",")
+	poll := creator(title, options)
+	res, err := json.MarshalIndent(poll, "", "\t")
+	if err != nil {
+		log.Panic(err)
+	}
+	return c.SendString(string(res))
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/ping", ping)
+	app.Post("/createPoll", createPoll)
+}
+
 func server() {
 	app := fiber.New()
-	log.Println("Server started!")
-	app.Get("/", func(c *fiber.Ctx) error {
-		msg, err := json.MarshalIndent("Voting Service. Version 0.0.1", "\t", "\t")
-		if err != nil {
-			log.Panic(err)
-		}
-		return c.SendString(string(msg))
-	})
-	app.Get("/createPoll", func(c *fiber.Ctx) error {
-		title := c.Query("title")
-		options := strings.Split(c.Query("options"), ",")
-		poll := creator(title, options)
-		res, err := json.MarshalIndent(poll, "", "\t")
-		if err != nil {
-			log.Panic(err)
-		}
-		return c.SendString(string(res))
-	})
-
+	setupRoutes(app)
 	log.Fatal(app.Listen(":8000"))
 }
