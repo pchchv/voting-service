0ec568021b2f260cc90a0970024af55a9953192e
pchchv <ipchchv@gmail.com>
2022-07-21 23:01:33 +0300

    Implement a server using the Fiber framework

--- server.go
+++ server.go
@@ -3,40 +3,31 @@ package main
 import (
 	"encoding/json"
 	"log"
-	"net/http"
 	"strings"
+
+	"github.com/gofiber/fiber/v2"
 )
 
-func ping(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	res, err := json.MarshalIndent("Voting Service. Version 0.0.1", "\t", "\t")
-	if err != nil {
-		log.Panic(err)
-	}
-	_, err = w.Write(res)
-	if err != nil {
-		log.Panic(err)
-	}
-}
-
-func createPoll(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Query().Get("title")
-	options := strings.Split(r.URL.Query().Get("options"), ",")
-	poll := creator(title, options)
-	res, err := json.MarshalIndent(poll, "", "\t")
-	if err != nil {
-		log.Panic(err)
-	}
-	_, err = w.Write(res)
-	if err != nil {
-		log.Panic(err)
-	}
-}
-
 func server() {
-	mux := http.NewServeMux()
+	app := fiber.New()
 	log.Println("Server started!")
-	mux.HandleFunc("/ping", ping)
-	mux.HandleFunc("/createPoll", createPoll)
-	log.Fatal(http.ListenAndServe(":8000", mux))
+	app.Get("/", func(c *fiber.Ctx) error {
+		msg, err := json.MarshalIndent("Voting Service. Version 0.0.1", "\t", "\t")
+		if err != nil {
+			log.Panic(err)
+		}
+		return c.SendString(string(msg))
+	})
+	app.Get("/createPoll", func(c *fiber.Ctx) error {
+		title := c.Query("title")
+		options := strings.Split(c.Query("options"), ",")
+		poll := creator(title, options)
+		res, err := json.MarshalIndent(poll, "", "\t")
+		if err != nil {
+			log.Panic(err)
+		}
+		return c.SendString(string(res))
+	})
+
+	log.Fatal(app.Listen(":8000"))
 }
